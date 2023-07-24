b69c920ac7e715bdbd1cd9b129c13ed1afa705f9
SoA-Lee <llsa0927@naver.com>
2023-07-24 14:39:19 +0900

    init

---
+++ src/main/java/com/hdc/labchelin/LabchelinApplication.java
@@ -0,0 +1,13 @@
+package com.hdc.labchelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabchelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabchelinApplication.class, args);
+	}
+
+}
